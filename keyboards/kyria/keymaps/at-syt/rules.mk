eb3a4f54283a068915a2cdc934998d1e251e0dd9
sebastian.seitner <sebastian.seitner@vbcf.ac.at>
2021-06-22 09:38:56 +0200

    Added custom keymap

---
+++ keyboards/kyria/keymaps/at-syt/rules.mk
@@ -0,0 +1,10 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+LEADER_ENABLE = yes        # Enable the Leader Key feature
+MOUSEKEY_ENABLE = no
+COMBO_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE   = no # Commands for debug and configuration
+UNICODE_ENABLE   = no# Unicode
+EXTRAFLAGS += -flto
